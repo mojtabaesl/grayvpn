41e7dbd9420bd5d2ccd8099f4df09d88e1395a85
mojtaba eslami <mojtabaeslami.72@gmail.com>
2023-02-22 16:42:32 +0330

    change xray config file path

--- commands/install-xray-config.sh
+++ commands/install-xray-config.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 _install-xray-config() {
-  local xrayConfigFile="./configs/xray-config.json"
+  local xrayConfigFile="~/xray/config.json"
   if [[ -f "$xrayConfigFile" ]]; then
     mkdir -p /etc/xray
     cp $xrayConfigFile /etc/xray/config.json
     _green "Installing xray config was successful"
   else
-    _red "Missing xray-config.json file! :(\n"
+    _red "Missing ~/xray/config.json file! :(\n"
     exit 0
   fi
 }
