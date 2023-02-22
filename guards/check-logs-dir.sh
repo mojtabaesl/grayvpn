97416fb292fd678d8761807bbb7d7168c70acb4f
mojtaba eslami <mojtabaeslami.72@gmail.com>
2023-02-23 02:13:26 +0330

    fix xray logs dir guard

--- guards/check-logs-dir.sh
+++ guards/check-logs-dir.sh
@@ -5,10 +5,6 @@ _import "/common/helpers.sh"
 _check-logs-dir() {
   if [[ ! -d "/etc/xray/logs" ]]; then
     mkdir -p /etc/xray/logs >/dev/null 2>&1
-    if [[ $? == 0 ]]; then
-      _success "Xray logs directory has been created successfully (/etc/xray/logs)"
-    fi
-    _error "Can not create directory /etc/xray/logs"
-    exit 0
+    _success "Xray logs directory has been created successfully (/etc/xray/logs)"
   fi
 }
